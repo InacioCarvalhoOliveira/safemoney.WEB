097c1a2c47d5bef931841e72bb9b80bf71e57265
InacioCarvalhoOliveira <inacio.carvalho.oliveira02@gmail.com>
2023-08-11 21:08:30 -0300

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace safemoney.WEB.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
